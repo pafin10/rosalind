130be44211b64c9db0873d23463bfb174fdabed0
mad_hatter <pafingl@gmail.com>
2023-05-21 17:55:44 +0200

    concise... learning proper python syntax is definitely worth it...

---
+++ src/Finding_shared_motiv.py
@@ -0,0 +1,45 @@
+def read_fasta(fastafile):
+    ids = []
+    sequences = []
+    sequence = ""
+
+    with open(fastafile, "r") as file:
+        for line in file:
+            line = line.strip()
+
+            if line.startswith(">"):
+                id = line[1:]
+                ids.append(id)
+
+                if sequence:
+                    sequences.append(sequence)
+                    sequence = ""
+
+            else:
+                sequence += line
+
+        # append last sequence
+        if sequence:
+            sequences.append(sequence)
+
+    return sequences
+
+
+def find_longest_substring(sequences):
+    longest_substring = ""
+    substring = ""
+
+    for i in range(len(sequences[0])):
+        for j in range(len(sequences[0]) + 1):
+            substring = sequences[0][i:j]
+            present_in_all = all(substring in seq for seq in sequences[1:])
+
+            if present_in_all and len(substring) > len(longest_substring):
+                longest_substring = substring
+
+    return longest_substring
+
+
+if __name__ == '__main__':
+    file = read_fasta("/Users/mad_hatter/PycharmProjects/rosalind/src/txt_files/rosalind_lcsm.txt")
+    print(find_longest_substring(file))
